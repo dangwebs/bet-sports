6b7ee7fad12a9518352862c2418fe346c9891684
Jhorman Orozco <jsojhor@gmail.com>
2026-01-06 16:18:17 -0500

    Update service worker precache revision for index.html.

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5cukso8gueo"
+    "revision": "0.992or98o8r"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
